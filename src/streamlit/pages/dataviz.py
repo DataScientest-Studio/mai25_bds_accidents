5b51c2da59f4e3a465bf03c1fda62b892ea67233
Alizée Blanchon <alizeeblanchon@yahoo.fr>
2025-07-13 23:29:15 +0200

    ok

--- src/streamlit/pages/dataviz.py
+++ src/streamlit/pages/dataviz.py
@@ -2,7 +2,19 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os 
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# remonter de 3 dossiers
+DATA_RAW_DIR = os.path.abspath(os.path.join(BASE_DIR, "..", "..", "..", "data", "raw"))
+DATA_PROCESSED_DIR = os.path.abspath(os.path.join(BASE_DIR, "..", "..", "..", "data", "processed"))
+
+usagers_path = os.path.join(DATA_RAW_DIR, "usagers-2019.csv")
+vehicules_path = os.path.join(DATA_RAW_DIR, "vehicules-2019.csv")
+lieux_path = os.path.join(DATA_RAW_DIR, "lieux-2019.csv")
+caracteristiques_path = os.path.join(DATA_RAW_DIR, "caracteristiques-2019.csv")
+accidents_path = os.path.join(DATA_PROCESSED_DIR, "accidents_clean.csv")
 
 def run():
     st.title("Visualisation des données")
@@ -10,7 +22,7 @@ def run():
 
     # Chargement des données nettoyées
     try:
-        df = pd.read_csv("data/processed/accidents_clean.csv")
+        df = pd.read_csv(accidents_path)
     except FileNotFoundError:
         st.error("Le fichier 'accidents_clean.csv' est introuvable.")
         return
--- src/streamlit/pages/deeplearning.py
+++ src/streamlit/pages/deeplearning.py
@@ -1,1 +1,14 @@
 
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# remonter de 3 dossiers
+DATA_RAW_DIR = os.path.abspath(os.path.join(BASE_DIR, "..", "..", "..", "data", "raw"))
+DATA_PROCESSED_DIR = os.path.abspath(os.path.join(BASE_DIR, "..", "..", "..", "data", "processed"))
+
+usagers_path = os.path.join(DATA_RAW_DIR, "usagers-2019.csv")
+vehicules_path = os.path.join(DATA_RAW_DIR, "vehicules-2019.csv")
+lieux_path = os.path.join(DATA_RAW_DIR, "lieux-2019.csv")
+caracteristiques_path = os.path.join(DATA_RAW_DIR, "caracteristiques-2019.csv")
+accidents_path = os.path.join(DATA_PROCESSED_DIR, "accidents_clean.csv")
